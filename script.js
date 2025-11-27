3a80e26b06026efa2752c33189f51b8dbdbdb236
Marcus <Helgesenhoff@gmail.com>
2025-11-27 08:14:00 +0100

    La til kommentarer

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
-﻿// Henter pakker.
+﻿// Importerer pakker.
 const express = require("express");
 const mysql = require("mysql2/promise");
-//Bruker express
+
 const app = express();
 
-//Skaper koblingen til databasen
-const { createConnection } = require("./database/database");
-const { getName } = require("./database/services");
 
-//tar i bruk ejs og express
+// Definerer hvilken port som skal være åpen for å motta forespørsler (req) fra klient.
+const port = 4000;
+
+// importerer funkjson som lager kobling til databasen.
+const { createConnection } = require("./database/database");
+// importerer funkjson som henter data fra databasen.
+const { getName } = require("./database/services");
+// konfigurerer EJS som malmotor.
 app.set("view engine", "ejs");
-//
+// serverer statiske filer.
 app.use(express.static("public"));
 
-//Async funksjon som viser hvor bruker skal få data?
+// forteller hvor landingssiden er
 app.get("/", async (req, res) => {
+  // åpner en ny mysql tilkobling
   const connection = await createConnection();
+  // henter data fra databasen.
   const results = await getName(connection);
+  // definerer hvordan vi skal svare på forsepørslen (req) fra klienten på denne ruten.
   res.render("index", {
     user: results,
     title: "Title",
     message: "Kattefakta for dagen",
   });
 });
-
+// Forteller hvordan siden/koden skal svare til en get forespørsel?
 app.get("/fakta", async (req, res) => {
   const catFact = await getCatFact();
-
+// definerer hvordan vi skal svare på forsepørslen (req) fra klienten på denne ruten.
   res.render("fact", {
     title: "Fakta",
     heading: "Velkommen til kattefakta",
     fact: catFact.fact,
   });
 });
-
+//En funksjon som henter data fra catfact
 async function getCatFact() {
   const response = await fetch("https://catfact.ninja/fact");
   const data = await response.json();
   return data;
 }
 
-const port = 4000;
+
 
 app.listen(port, (req, res) => {
   console.log(`"Denne serveren kjører på"${port}`);
